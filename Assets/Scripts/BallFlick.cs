d675e56ca049737ed1175697d742a2deb2bf7ddf
Jen <bourkj@rpi.edu>
2021-03-25 14:12:41 -0400

    Longer press lessens force added

--- Assets/Scripts/BallFlick.cs
+++ Assets/Scripts/BallFlick.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class BallFlick : MonoBehaviour
 {
     //mouseTestVars
-    [SerializeField] private Camera _camera = null;
+    [SerializeField] private float _thrust = 20f;
+ 
+    private Transform _transform = null;
     private Vector2 _mousePosition0 = Vector2.zero;
     private Vector2 _mousePosition1 = Vector2.zero;
     private bool _flicked = false;
     private Rigidbody _rigidBody = null;
+    private float _timeHeld = 0f;
 
     void Awake()
     {
         _rigidBody = GetComponent<Rigidbody>();
+        _transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -38,17 +42,25 @@ public class BallFlick : MonoBehaviour
         if(_flicked == false){
             if (Input.GetMouseButtonDown(0)) {
                 _mousePosition0 = Input.mousePosition;
-                print(_mousePosition0);
+                _transform.GetComponent<Renderer>().material.color = Color.white;
+                //print(_mousePosition0);
             } 
+            if(Input.GetMouseButton(0)) {
+                _timeHeld += Time.deltaTime;
+                print(_timeHeld);
+            }
+
             if (Input.GetMouseButtonUp(0)) {
                 _mousePosition1 = Input.mousePosition;
-                print(_mousePosition1);
+                //print(_mousePosition1);
+                _transform.GetComponent<Renderer>().material.color = Color.black;
                 _flicked = true;
             }
         }else{
             Vector3 direction = new Vector3(_mousePosition1.x - _mousePosition0.x, _mousePosition1.y - _mousePosition0.y, 0);
-            print(_mousePosition1.x - _mousePosition0.x);
-            _rigidBody.AddForce(direction * 0.5f);
+            print(_timeHeld * _thrust);
+            _rigidBody.AddForce(direction * (2 - _timeHeld) * _thrust);
+            _timeHeld = 0;
             _flicked = false;
         }
     }
