74a8eada83a52f0a135133f3c073dc827038f1c6
Jen <bourkj@rpi.edu>
2021-03-26 14:43:58 -0400

    Add WinZone scripts
    Swaps levels when you win

---
+++ Assets/Scripts/WinZone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinZone : MonoBehaviour
+{
+    [SerializeField] private string sceneName = "Title";
+
+    // Update is called once per frame
+    void SceneSwitch()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    void OnTriggerEnter()
+    {
+        SceneSwitch();
+    }
+}
